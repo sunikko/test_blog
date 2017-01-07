7f966b03d36b12b0cd8398f075d2a6581e67ad8c
Oxbridge <oxbridge@MYOUNGHI-SONG-NAMs-MacBook-Pro.local>
2017-01-07 23:29:51 +0000

    refactor controller : seperate controller code from urls.py

--- soyblog/soyblog/urls.py
+++ soyblog/soyblog/urls.py
@@ -1,15 +1,8 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-from django.http.response import HttpResponse
+from soyblog.controller import home,room
 
-
-def home(request):
-    return HttpResponse("hello world")
-
-
-def room(request, room_id):
-    return HttpResponse("This is my room detail" + room_id)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
