ff18db28360512a035bd66e5d0251625a7995946
Oxbridge <oxbridge@MYOUNGHI-SONG-NAMs-MacBook-Pro.local>
2017-01-08 14:07:47 +0000

    init news view: render watcha news as html

--- soyblog/soyblog/urls.py
+++ soyblog/soyblog/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-from soyblog.views import home,room
+from soyblog.views import home,room,news
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', home),
     url(r'^rooms/(?P<room_id>\d+)/$', room),
+    url(r'^news/$', news),
 ]
--- soyblog/soyblog/views.py
+++ soyblog/soyblog/views.py
@@ -1,5 +1,8 @@
 import requests
+import json
+
 from django.http.response import HttpResponse
+
 
 def home(request):
     return HttpResponse("hello world")
@@ -12,3 +15,22 @@ def room(request, room_id):
     content_type = "application/json",
     )
 
+
+def news(request):
+    search = request.GET.get("search")
+
+    response = requests.get("https://watcha.net/home/news.json?page=1&per=50")
+    news_dict = json.loads(response.text)
+
+    content = "<h1>News</h1>" + \
+        "<p>This is news page.</p>" + \
+        "".join([
+            "<h2>{title}</h2><img src={img_src}><p>{content}</p>".format(
+                title=news.get("title"),
+                img_src = news.get('image'),
+                content = news.get('content'),
+            )
+            for news
+            in news_dict["news"]
+        ])
+    return HttpResponse(content,)
